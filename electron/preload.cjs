6dbe738758b4ef4883238d1f4fba8fdd03415de9
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-19 19:03:52 -0300

    feat: expose updater controls through preload bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,1 +1,15 @@
-const {contextBridge,ipcRenderer}=require('electron');const call=(n,p)=>ipcRenderer.invoke(`artisys:${n}`,p);contextBridge.exposeInMainWorld('artisys',Object.freeze(Object.fromEntries(['describe','authState','bootstrap','login','validate','logout','load','action'].map(n=>[n,p=>call(n,p)]))));
+const {contextBridge,ipcRenderer}=require('electron');
+const call=(n,p)=>ipcRenderer.invoke(`artisys:${n}`,p);
+const updates=Object.freeze({
+  state:()=>ipcRenderer.invoke('artisys:updates:state'),
+  check:()=>ipcRenderer.invoke('artisys:updates:check'),
+  download:()=>ipcRenderer.invoke('artisys:updates:download'),
+  install:()=>ipcRenderer.invoke('artisys:updates:install'),
+  onStatus:handler=>{
+    if(typeof handler!=='function')throw new TypeError('handler must be a function');
+    const listener=(_event,state)=>handler(state);
+    ipcRenderer.on('artisys:updates:status',listener);
+    return()=>ipcRenderer.removeListener('artisys:updates:status',listener);
+  }
+});
+contextBridge.exposeInMainWorld('artisys',Object.freeze({...Object.fromEntries(['describe','authState','bootstrap','login','validate','logout','load','action'].map(n=>[n,p=>call(n,p)])),updates}));
